667a44889298400c62589689588fbac4aefe98d8
guri670 <kory0485@gmail.com>
2025-02-24 00:59:04 +0900

    ci/cd 구축

--- src/main/java/com/koreait/short_url_project/HomeController.java
+++ src/main/java/com/koreait/short_url_project/HomeController.java
@@ -1,5 +1,6 @@
 package com.koreait.short_url_project;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,9 +8,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
+    @Value("${custom.site.name}")
+    private String customSiteName;
+
+    @Value("${custom.secret.key}")
+    private String secretKey;
+
     @GetMapping("/")
     @ResponseBody
     public String home() {
-        return "Main Page!";
+        return "Main Page! " + customSiteName;
+    }
+    @GetMapping("/secretKey")
+    @ResponseBody
+    public String secretKey() {
+        return "secretKey :  " + secretKey;
     }
 }
