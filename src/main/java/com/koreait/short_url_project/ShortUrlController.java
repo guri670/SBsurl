195b02bd63464c65376d6f5fbd1346f4a6fae450
guri670 <kory0485@gmail.com>
2025-02-23 23:53:17 +0900

    빌드

---
+++ src/main/java/com/koreait/short_url_project/ShortUrlController.java
@@ -0,0 +1,28 @@
+package com.koreait.short_url_project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class ShortUrlController {
+
+    private List<Surl> surls = new ArrayList<>();
+    private long surlLastId;
+
+    @GetMapping("/add")
+    @ResponseBody
+    public Surl add(String body, String url) {
+        Surl surl = Surl.builder()
+                .id(++surlLastId)
+                .body(body)
+                .url(url)
+                .build();
+        surls.add(surl);
+
+        return surl;
+    }
+}
